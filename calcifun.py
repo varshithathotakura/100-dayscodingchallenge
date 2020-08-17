ac7952aabbc0700c2ff35423fd79571cd340f1fb
varshithathotakura <52576621+varshithathotakura@users.noreply.github.com>
2020-08-17 12:27:43 +0530

    Add files via upload

---
+++ calcifun.py
@@ -0,0 +1,14 @@
+a=int(input("enter a num"))
+b=int(input("enter a num"))
+def add():
+    return(a+b)
+print("add:",add())
+def sub():
+    return(a-b)
+print("sub:",sub())
+def mul():
+    return(a*b)
+print("mul:",mul())
+def div():
+    return(a/b)
+print("div:",div())
